049fba5235f4a31aa78b609781c2631250ced5ed
Sakura-501 <2359216537@qq.com>
2024-01-06 13:45:20 +0800

    mongo_finish

--- darkbot/tools/create_mongo.py
+++ darkbot/tools/create_mongo.py
@@ -6,8 +6,8 @@ config_all = config_all()
 
 class mongo_latest:
     def __init__(self):
-        mongo_url = "mongodb://{}:{}@{}:{}/".format(config_all.mongo_username,config_all.mongo_password,config_all.mongo_host, config_all.mongo_port)
-        self.client = pymongo.MongoClient(mongo_url)
+        # mongo_url = "mongodb://{}:{}@{}:{}/?authSource={}".format(config_all.mongo_username,config_all.mongo_password,config_all.mongo_host, config_all.mongo_port,config_all.mongo_database)
+        self.client = pymongo.MongoClient(host=config_all.mongo_host,port=int(config_all.mongo_port),username=config_all.mongo_username,password=config_all.mongo_password)
         self.db = self.client[config_all.mongo_database]
 
     # def create_onion_list_collection(self):
